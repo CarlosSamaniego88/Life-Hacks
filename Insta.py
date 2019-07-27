4f955ec7d4985154e8d4f4999c33f1efbdd4c5ca
Carlos Samaniego <Carlos@carloss-mbp.home>
2019-07-27 15:27:26 -0400

    current progress

--- Insta.py
+++ Insta.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from instapy_cli import client
+from os import listdir
+from os.path import isfile, join
+
+username = "USERNAME"
+password = "PASSWORD"
+image = "FILE PATH"
+text = "Uploaded via python"
+mypath = "/Users/Carlos/Projects/Life-Hacks/future_posts"
+
+future_posts = [photo for photo in listdir(mypath) if isfile(join(mypath, photo))]
+print(future_posts)
+
+for post in range(len(future_posts)):
+    with client(username, password) as cli:
+        cli.upload(post, text)
+        print("success")
