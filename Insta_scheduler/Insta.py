59b16270448c1bc2b96eaa630ca0cf6b4687a01c
Carlos Samaniego <Carlos@carloss-mbp.home>
2019-07-27 15:30:06 -0400

    folder restructuring

--- Insta_scheduler/Insta.py
+++ Insta_scheduler/Insta.py
@@ -6,7 +6,7 @@ username = "USERNAME"
 password = "PASSWORD"
 image = "FILE PATH"
 text = "Uploaded via python"
-mypath = "/Users/Carlos/Projects/Life-Hacks/future_posts"
+mypath = "/Users/Carlos/Projects/Life-Hacks/Insta_scheduler/future_posts"
 
 future_posts = [photo for photo in listdir(mypath) if isfile(join(mypath, photo))]
 print(future_posts)
