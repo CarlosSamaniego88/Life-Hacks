71585b6581d419b6f808e8789c06f4d7fcdd897d
Carlos Samaniego <Carlos@Carloss-MacBook-Pro.local>
2019-08-06 16:36:45 -0400

    scheduling complete

--- Insta_scheduler/Insta.py
+++ Insta_scheduler/Insta.py
@@ -7,23 +7,26 @@ def main():
     post()
 
 def post():
-    username = "user"
-    password = "pass"
-    mypath = "/Users/Carlos/Projects/Life-Hacks/Insta_scheduler/future_posts/*.JPG"
+    username = "USERNAME"           #insert your username  
+    password = "PASSWORD"           #insert your password
+    mypath = "/Users/Carlos/Projects/Life-Hacks/Insta_scheduler/future_posts/*.JPG"     #insert path to folder with photos you want to post
 
-    image_paths = glob.glob(mypath)
+    image_paths = glob.glob(mypath)     #will iterate and store all image paths in folder
+    # print(image_paths)
     insta_post_descriptions = ["TBT to Chihuahua when all I just worried about was tortas piolines #TortasPiolines #python",
                                 "TBT to spring break with the day one fam #wynwood #python"
-                            ]
-    future_posts = zip(image_paths, insta_post_descriptions)
-    print(future_posts)
+                            ]       #associate a description with your photo, check order with print statement in 15 
+    
+    future_posts = zip(image_paths, insta_post_descriptions)    #creates a dictionary with photo and description
+    # print(future_posts)
    
     with client(username, password) as cli:
         for photo, description in future_posts: 
             cli.upload(photo, description)
+            break                               #break allows the posting of one photo at a time instea of all in folder
         print("success")
 
-schedule.every().tuesday.at("16:23").do(post)
+schedule.every().thursday.at("15:00").do(post)  #insert when you want to schedule posts
 
 while True:
     schedule.run_pending()
