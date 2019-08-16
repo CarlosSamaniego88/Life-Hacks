527360c623ad221b4628809c7018c8d5383d5ec4
Carlos Samaniego <cscarlossamaniego@gmail.com>
2019-08-15 21:42:21 -0400

    started implementing imap

--- email_text_alerts/emtext_alerts.py
+++ email_text_alerts/emtext_alerts.py
@@ -1,8 +1,23 @@
 from twilio.rest import Client
+from imapclient import IMAPClient
 import os
 
 def main():
-    sendText()
+    server = IMAPClient('imap.gmail.com', use_uid=True)
+    GMAIL_PASS = os.environ['GMAIL_PASS']
+    server.login('cscarlossamaniego@gmail.com', GMAIL_PASS)
+
+    select_info = server.select_folder('INBOX')
+    #print('%d messages in INBOX' % select_info[b'EXISTS'])
+
+    messages = server.search(['FROM', 'samaca16@wfu.edu'])
+    print("%d messages from our my school account" % len(messages))
+
+    for msgid, data in server.fetch(messages, ['ENVELOPE']).items():
+        envelope = data[b'ENVELOPE']
+        print('"%s"' % (envelope.subject.decode()))
+
+    #sendText()
 
 def sendText():
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
